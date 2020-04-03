e801e29c5c920efc825e59199ed25a1a5bb0bfd4
yasslkb <47532104+yasslkb@users.noreply.github.com>
2020-04-03 03:24:14 +0100

    Update liste-adjacence.cpp

--- liste-adjacence.cpp
+++ liste-adjacence.cpp
@@ -1,37 +1,98 @@
 #include<iostream>
-#include<vector>
-#include<iterator >
-using namespace std  ;
-	void ajouterarc(vector<int>graphe[] , int u,int v){
-		graphe[u].push_back(v);
-		graphe[v].push_back(u);
-	} 
-	void print(vector<int>graphe[],int n){
-		vector<int> :: iterator it ; 
-		cout<<"************************************"<<endl ; 
-		for(int i=0;i<n;i++){
-			cout<<"la liste d'adjacence de : "<<i <<endl ; 
-			for(it=graphe[i].begin();it!=graphe[i].end();it++){
-				cout<<*it<<'\t';
-				cout<<'\n';
-			}
-		}
-	}
-int main(){
-	int n , e ,u,v ; 
-	cout<<"entrez le nombre de sommets : " ; 
-	cin>>n ; 
-	cout<<"entrez le nombre d arrets  ";
-	cin>>e ; 
-	vector <int> graphe[n] ;
-	
-	for(int i=0;i<e ; i++){
-		cout<<"saisir le debut et la fin d'arret : \n ";
-		cin>>u ; 
-		cin>>v ; 
-		ajouterarc(graphe,u,v) ; 
-	}
-	print(graphe,n) ; 
+using namespace std;
 
-	
+class sommet {
+public:
+    int valsom;
+    sommet* suivant;
+};
+
+class arret {
+public:
+    int source, destination;
+
+};
+
+class graph {
+    int N; //nb des sommets.
+    sommet* neveau_Sommet(int, sommet*);
+public:
+    sommet** tete;
+    graph(arret*, int, int);
+    ~graph();
+
+
+sommet* neveau_Sommet(int val_S, sommet* tete) {
+    sommet* neveauS = new sommet;
+    neveauS->valsom = val_S;
+    neveauS->suivant = tete;
+    return neveauS;
+}
+
+graph(arret* Arrets, int n, int N) {
+    this->N = N;
+    tete = new sommet * [N];
+    for (int i = 0; i < N; i++) {
+        tete[i] = nullptr;
+
+        for (int i = 0; i < n; i++) {
+            int source = Arrets[i].source;
+            int destination = Arrets[i].destination;
+
+            sommet* neveauS = neveau_Sommet(destination, tete[source]);
+            
+            tete[source] = neveauS;
+            sommet* neveauS1 = neveau_Sommet(source, tete[destination]);
+            tete[destination] = neveauS1;
+        }
+    }
+}
+
+~graph() {
+    for (int i = 0; i < N; i++) {
+        delete tete[i];
+    }
+    delete tete;
+  
+}
+};
+
+void afficherlistevoisin(sommet* vs) {
+    while (vs != nullptr) {
+        cout << vs->valsom << "->";
+        vs = vs->suivant;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // array of graph edges as per above diagram.
+    arret edges[] =
+    {
+        // pair (x, y) represents edge from x to y
+        { 0, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 },
+        { 3, 2 }, { 4, 5 }, { 5, 4 },{2,3},{2,5}
+    };
+
+    // Number of vertices in the graph
+    int N = 6;
+
+    // calculate number of edges
+    int n = sizeof(edges) / sizeof(edges[0]);
+
+    // construct graph
+    graph GA(edges, n, N);
+
+    // print adjacency list representation of graph
+    for (int i = 0; i < N; i++)
+    {
+        // print given vertex
+        cout << i << " --";
+
+        // print all its neighboring vertices
+        afficherlistevoisin(GA.tete[i]);
+    }
+
+    return 0;
 }
